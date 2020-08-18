aaaeb4f589839fb811fb08bb138ccd3a78668720
SoulMan87 <jonathanhinestroza87@gmail.com>
2020-08-17 20:47:04 -0500

    validadores

---
+++ src/app/form-destino-viaje/form-destino-viaje.component.ts
@@ -0,0 +1,33 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {DestinoViaje} from "../models/destino-viaje.model";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-form-destino-viaje',
+  templateUrl: './form-destino-viaje.component.html',
+  styleUrls: ['./form-destino-viaje.component.css']
+})
+export class FormDestinoViajeComponent implements OnInit {
+ @Output()onItemAdded:EventEmitter<DestinoViaje>;
+ fg: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.onItemAdded = new EventEmitter();
+    this.fg = fb.group({
+      nombre:['', Validators.required],
+      url:['']
+    });
+    this.fg.valueChanges.subscribe((form:any)=>{
+      console.log('Cambio el formulario: ');
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  guardar(nombre:string, url:string):boolean {
+    const d = new DestinoViaje(nombre, url);
+    this.onItemAdded.emit(d);
+    return false;
+  }
+}
